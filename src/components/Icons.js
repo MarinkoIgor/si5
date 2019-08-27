aa467e212ff610341b22a027a1eb6f363fab3708
rogyim <imarinkovic@sabaltech.com>
2019-08-27 02:21:51 +0200

    icons

---
+++ src/components/Icons.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const ModuleIcon = props => (
+  <img {...props} src={require('../assets/module.svg')} alt="Module icon" />
+)
+
+export const ArrowDownIcon = props => (
+  <img {...props} src={require('../assets/arrow_down.svg')} alt="Arrow down icon" />
+)
+
+export const ArrowUpIcon = props => (
+  <img {...props} src={require('../assets/arrow_up.svg')} alt="Arrow up icon" />
+)
+
+export const ReorderIcon = props => (
+  <img {...props} src={require('../assets/reorder.svg')} alt="Reorder icon" />
+)
+
+export const LessonIcon = props => (
+  <img {...props} src={require('../assets/lesson.svg')} alt="Lesson icon" />
+)
+
+export const ArchivedIcon = props => (
+  <img {...props} src={require('../assets/archived.svg')} alt="Reload icon" />
+)
